1d2df907ec1c1a1bccab29ccfeeb37daa7fe5fa5
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-04-21 12:49:53 +0200

    aaa

--- fizzbuzztest.cpp
+++ fizzbuzztest.cpp
@@ -10,6 +10,9 @@ protected:
 		//game_initialize();	/* Without this the Tests could break*/
 	}
 };
+
+// Funktionen behöver inte ha en return för att vara testbar?
+// TIPS: Dela upp er kod! Testbar funktion -> bara beräkningar, inga scanf/printf
 TEST_F(FizzbuzzTest,WhenTalIsDivisibleBy3and5ThenFizzBuzzShouldBeReturned){
     //ARRANGE
     int tal = 30;
@@ -19,6 +22,42 @@ TEST_F(FizzbuzzTest,WhenTalIsDivisibleBy3and5ThenFizzBuzzShouldBeReturned){
     //ASSERT
     ASSERT_STREQ("FizzBuzz", buffer);
 }
+
+//Om tal jämt delbart med 3 och 5 -> "fizzbuzz"
+//Om tal jämt delbart med 3 -> "fizz"
+TEST_F(FizzbuzzTest,WhenTalIsDivisibleBy3ThenFizzShouldBeReturned){
+    //ARRANGE
+    int tal = 6;
+    char buffer[10];
+    //ACT
+    calculateFizzBuzz(tal, buffer);
+    //ASSERT
+    ASSERT_STREQ("Fizz", buffer);
+}
+
+//Om tal jämt delbart med 5 -> "buzz"
+TEST_F(FizzbuzzTest,WhenTalIsDivisibleBy10ThenBuzzShouldBeReturned){
+    //ARRANGE
+    int tal = 10;
+    char buffer[10];
+    //ACT
+    calculateFizzBuzz(tal, buffer);
+    //ASSERT
+    ASSERT_STREQ("Buzz", buffer);
+}
+
+
+// annars talet som sträng
+TEST_F(FizzbuzzTest,WhenTalIsNotDivisableBy3or5ThenNumberShouldBeReturned){
+    //ARRANGE
+    int tal = 8;
+    char buffer[10];
+    //ACT
+    calculateFizzBuzz(tal, buffer);
+    //ASSERT
+    ASSERT_STREQ("8", buffer);
+}
+
 
 
 
