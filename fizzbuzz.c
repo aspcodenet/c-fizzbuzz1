c5496919da8c19e1bc2812749d7c960df24f3833
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-04-21 10:20:34 +0200

    a

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,5 +1,15 @@
+#include <string.h>
+#include <stdio.h>
 #include "fizzbuzz.h"
 
-void calculateFizzBuzz(int tal, char *szOutBuffer){
 
+//Om tal jämt delbart med 3 och 5 -> "fizzbuzz"
+//Om tal jämt delbart med 3 -> "fizz"
+//Om tal jämt delbart med 5 -> "buzz"
+// annars talet som sträng
+void calculateFizzBuzz(int tal, char *szOutBuffer){
+    if( tal % 5 == 0 && tal % 3 == 0  ) strcpy(szOutBuffer,"FizzBuzz");
+    else if(tal % 3 == 0  ) strcpy(szOutBuffer,"Fizz");
+    else if(tal % 5 == 0  ) strcpy(szOutBuffer,"Buzz");
+    else sprintf(szOutBuffer,"%d", tal);
 }
